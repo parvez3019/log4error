6519eecff16d0b102af520614a55cf30a520996f
parvez3019 <pha3019@gmail.com>
2024-03-27 12:24:07 +0530

    Add logger filter example

---
+++ src/main/java/io/github/parvez3019/LoggerFilterExample.java
@@ -0,0 +1,45 @@
+package io.github.parvez3019;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@Order(1)
+public class LoggerFilterExample extends OncePerRequestFilter {
+    private static final String X_REQUEST_ID = "x-request-id";
+    private static final LoggerThreadLocal requestLogInfoThreadLocal = new LoggerThreadLocal();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        boolean headerFound = StringUtils.hasLength(request.getHeader(X_REQUEST_ID));
+        String requestID = headerFound ? request.getHeader(X_REQUEST_ID) : UUID.randomUUID().toString();
+        MDC.put(X_REQUEST_ID, requestID);
+        requestLogInfoThreadLocal.set(new Logger());
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.remove(X_REQUEST_ID);
+            requestLogInfoThreadLocal.remove();
+        }
+    }
+
+    /**
+     * @return will return you the logger instance that can statically imported in your classes.
+     */
+    public static Logger Logger() {
+        return requestLogInfoThreadLocal.getLogger();
+    }
+
+}
+
